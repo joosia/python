f26cb8594fe1c8f5c12d167313405c7cd5ad9ae6
joosia <30998472+joosia@users.noreply.github.com>
2018-01-21 18:41:32 +0200

    Fixing git connection issues

---
+++ 23-29/A24.py
@@ -0,0 +1,10 @@
+
+def calculator (str, num1, num2):
+    if str == "add":
+        return num1 + num2
+    elif str == "sub":
+        return num1 - num2
+    elif str == "multiply":
+        return num1 * num2
+    else:
+        return None
